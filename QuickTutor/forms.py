c72e44ad52c2124aa3a52dc4f243b8e70d0c9b63
Sammie (Ella) Chung <sc6aa@virginia.edu>
2020-04-21 21:53:21 -0400

    Inserted datepicker via CDN, commented out relevant parts

--- QuickTutor/forms.py
+++ QuickTutor/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import QTUser, Review, Class, ClassNeedsHelp, Session, TutorableClass
+#from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
 
 class ClassNeedsHelpForm(forms.ModelForm):
     class Meta:
@@ -88,4 +89,47 @@ class EditProfileForm(forms.ModelForm):
         model = QTUser
         fields = ['first_name','last_name','year','rough_payment_per_hour','rough_willing_to_pay_per_hour']
 
-    
+'''
+class MyForm(forms.Form):
+    date_field = forms.DateField(widget=DatePicker())
+    date_field_required_with_min_max_date = forms.DateField(
+        required=True,
+        widget=DatePicker(
+            options={
+                'minDate': '2009-01-20',
+                'maxDate': '2017-01-20',
+            },
+        ),
+        initial='2013-01-01',
+    )
+    """
+    In this example, the date portion of `defaultDate` is irrelevant;
+    only the time portion is used. The reason for this is that it has
+    to be passed in a valid MomentJS format. This will default the time
+    to be 14:56:00 (or 2:56pm).
+    """
+    time_field = forms.TimeField(
+        widget=TimePicker(
+            options={
+                'enabledHours': [9, 10, 11, 12, 13, 14, 15, 16],
+                'defaultDate': '1970-01-01T14:56:00'
+            },
+            attrs={
+                'input_toggle': True,
+                'input_group': False,
+            },
+        ),
+    )
+    datetime_field = forms.DateTimeField(
+        widget=DateTimePicker(
+            options={
+                'useCurrent': True,
+                'collapse': False,
+            },
+            attrs={
+                'append': 'fa fa-calendar',
+                'icon_toggle': True,
+            }
+        ),
+    )
+'''
